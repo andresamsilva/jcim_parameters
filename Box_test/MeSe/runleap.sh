36e1808cb9d16b611a948504ddb3894f40b47e97
andresamsilva <andresamessias4@gmail.com>
2022-12-06 20:29:31 -0300

    Water Box size

---
+++ Box_test/MeSe/runleap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ~/Programs/amber18/amber.sh
+res="MSE"
+
+# source leaprc.ff14SB for amber < 16. 
+# If not, source leaprc.protein.ff14SB
+
+echo "source leaprc.protein.ff14SB
+      source leaprc.water.tip3p
+      loadamberparams MES.prm
+      loadamberprep MES.in
+      mes = loadpdb mes.pdb 
+      charge mes
+      solvatebox mes TIP3PBOX 10
+      saveAmberParm mes $res_box.prmtop $res_box.rst7
+      savepdb mes ${res}_box.pdb
+      quit" > lista-tleap
+
+tleap -f lista-tleap
+rm lista-tleap
+
