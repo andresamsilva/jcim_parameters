c4494e65bfd3f08b51a0857a6f2a21b89ff2fa66
andresamsilva <andresamessias4@gmail.com>
2022-12-06 20:01:13 -0300

    species simulation

---
+++ LJ_fit/MeSSH/amber_simulation/runleap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ~/Programs/amber18/amber.sh
+res="SSH"
+
+# source leaprc.ff14SB for amber < 16. 
+# If not, source leaprc.protein.ff14SB
+
+echo "source leaprc.protein.ff14SB
+      source leaprc.water.tip3p
+      loadAmberParams $res.prm
+      loadAmberPrep   $res.in
+      mol = sequence { $res }
+      charge mol
+      solvateOct mol TIP3PBOX 5.3
+      saveAmberParm mol $res.prmtop $res.rst7
+      savepdb mol ${res}.pdb
+      quit" > lista-tleap
+
+tleap -f lista-tleap
+rm lista-tleap
+
